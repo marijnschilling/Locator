449300cab6278125abb22d151f8ab257dd8d6125
Elvis Nuñez <elvisnunez@me.com>
2015-12-03 01:07:51 +0100

    Add annotations to map

--- iOS/Source/MapController.swift
+++ iOS/Source/MapController.swift
@@ -1,5 +1,6 @@
 import UIKit
 import MapKit
+import CoreData
 
 class MapController: BaseViewController {
     override func loadView() {
@@ -11,5 +12,15 @@ class MapController: BaseViewController {
         super.viewDidLoad()
 
         self.title = "Map"
+
+        let fetchRequest = NSFetchRequest(entityName: Venue.entityName())
+        let items = try! self.fetcher.context.executeFetchRequest(fetchRequest) as! [Venue]
+        let mapView = self.view as! MKMapView
+        for item in items {
+            let annotation = MKPointAnnotation()
+            annotation.title = item.title
+            annotation.coordinate = CLLocationCoordinate2D(latitude: Double(item.latitude!)!, longitude: Double(item.longitude!)!)
+            mapView.addAnnotation(annotation)
+        }
     }
 }
